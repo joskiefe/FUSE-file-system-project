1ad435dee5bdc4c07c463067a88917e3f33f2606
joskiefe <josh9041776@gmail.com>
2019-06-15 15:06:55 -0400

    Update giegertimestamps.py
    added my week one implementation for the Geigertimestamps.py file this does an iostream from our GeigerCounter and saves directly to timestamp.txt file in directory, sorry for such a long delay, just hadnt updated anything in so long here figured i'd just keep this up to date.

--- giegertimestamps.py
+++ giegertimestamps.py
@@ -1,2 +1,19 @@
 #Program to export timestamps from Raspbery Pi pin 23 off Gieger Counter to FUSE system
 #giegertimestamps.py
+#=============================timestamp collection==========================
+#Author: Joshua Kiefer
+#!/usr/bin/env python
+import pigpio,time
+
+razpi1 = pigpio.pi()
+razpi1.set_mode(23,pigpio.INPUT)               #set pin23 as input
+time.sleep(5)                                                  
+
+while 1:
+
+    if razpi1.wait_for_edge(23, pigpio.FALLING_EDGE, 5.0):      #if event occurs on pin23
+        timeStamp = time.time()                                                      #create time stamp
+        time_stamp_File_Write = open('timestamp.txt', 'a')             #open/create timestamp.txt 
+        time_stamp_File_Write.write((str(timeStamp)) + '\n')          #write/append file
+        time_stamp_File_Write.flush()                                             #clear fstream buffer
+=====================================================================
